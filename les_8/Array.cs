2ce251a8e3736c58b35dabf3ae7e3687d2a589f4
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-09-27 20:02:56 +0300

    Add project files.

---
+++ les_8/Array.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les_8
+{
+    internal class Array<T> where T : struct
+    {
+        public int Size { get; set; }
+        private T[] _arr;
+        public Array(int size)
+        {
+            _arr = new T[size];
+            Size = size;
+        }
+        public T this[int i]
+        {
+            get { return _arr[i]; }
+
+            set { _arr[i] = value; }
+        }
+        public static Array<T> operator +(Array<T> a, Array<T> b)
+        {
+            Array<T> result = new Array<T>(a.Size);
+
+            for (int i = 0; i < a.Size; ++i)
+            {
+                result[i] = (dynamic)a[i] + b[i];
+            }
+
+            return result;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Size; ++i)
+            {
+                sb.Append(this[i]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ les_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array<int> arr1 = new Array<int>(9);
+            for (int i = 0; i < arr1.Size; ++i)
+            {
+                arr1[i] = i;
+            }
+            Console.WriteLine($"arr1: {arr1}\n");
+
+            Array<int> arr2 = new Array<int>(9);
+            for (int i = 0; i < arr2.Size; ++i)
+            {
+                arr2[i] = i + 3;
+            }
+            Console.WriteLine($"arr2: {arr2}\n");
+
+            Array<int> arr3 = arr1 + arr2;
+            Console.WriteLine($"arr3: {arr3}\n");
+        }
+    }
+}
